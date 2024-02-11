acaba9bb701aecc8332a817cc0ecccbba7d681a7
GabrielAgundiz <gabrielg.agundizh@gmail.com>
2024-02-11 02:37:33 -0600

    Shit Cleaning

--- src/fr/components/Footer.js
+++ src/fr/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import "./css/footer.css";
-import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
 import { FaThreads, FaXTwitter } from "react-icons/fa6";
 function FooterFR() {
     return (
