ead6610f25f567675e0e16068577965d717f6750
GabrielAgundiz <gabrielg.agundizh@gmail.com>
2024-02-11 01:43:46 -0600

    Navbar Fixes

--- src/en/components/LanguageSelector.jsx
+++ src/en/components/LanguageSelector.jsx
@@ -5,19 +5,19 @@ import { useLanguage } from '../../LanguageContext';
 import Flag from 'react-world-flags'
 
 const LanguageSelectorEN = () => {
-    const { language, changeLanguage } = useLanguage();
-    const navigate = useNavigate();
-    const location = useLocation();
-  
-    const handleLanguageChange = (newLanguage) => {
-      changeLanguage(newLanguage);
-      
-      const currentPath = location.pathname;
-      const newPath = currentPath.replace(`/${language}/`, `/${newLanguage}/`);
-      
-      navigate(newPath);
-    };
-  
+  const { language, changeLanguage } = useLanguage();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleLanguageChange = (newLanguage) => {
+    changeLanguage(newLanguage);
+
+    const currentPath = location.pathname;
+    const newPath = currentPath.replace(`/${language}/`, `/${newLanguage}/`);
+
+    navigate(newPath);
+  };
+
 
   return (
     <Dropdown>
@@ -25,10 +25,10 @@ const LanguageSelectorEN = () => {
         {language.toUpperCase()}
       </Dropdown.Toggle>
       <Dropdown.Menu class="dropdown-menu show" data-bs-popper="static">
-        <Dropdown.Item class="dropdown-item" onClick={() => handleLanguageChange('en')} style={{color: 'black'}}>English <Flag code="US" height="12" style={{maxHeight:'12px'}} /></Dropdown.Item>
-        <Dropdown.Item class="dropdown-item" onClick={() => handleLanguageChange('fr')} style={{color: 'black'}}>Français <Flag code="FR" height="12" style={{maxHeight:'12px'}} /></Dropdown.Item>
-        <Dropdown.Item class="dropdown-item" onClick={() => handleLanguageChange('es')} style={{color: 'black'}}>Español <Flag code="ES" height="12" style={{maxHeight:'12px'}} /></Dropdown.Item>
-        <Dropdown.Item class="dropdown-item" onClick={() => handleLanguageChange('de')} style={{color: 'black'}}>Deutsch <Flag code="DE" height="12" style={{maxHeight:'12px'}} /></Dropdown.Item>
+        <Dropdown.Item class="dropdown-item" onClick={() => handleLanguageChange('en')} style={{ color: 'black' }}>English <Flag code="US" height="12" style={{ maxHeight: '12px' }} /></Dropdown.Item>
+        <Dropdown.Item class="dropdown-item" onClick={() => handleLanguageChange('fr')} style={{ color: 'black' }}>Français <Flag code="FR" height="12" style={{ maxHeight: '12px' }} /></Dropdown.Item>
+        <Dropdown.Item class="dropdown-item" onClick={() => handleLanguageChange('es')} style={{ color: 'black' }}>Español <Flag code="ES" height="12" style={{ maxHeight: '12px' }} /></Dropdown.Item>
+        <Dropdown.Item class="dropdown-item" onClick={() => handleLanguageChange('de')} style={{ color: 'black' }}>Deutsch <Flag code="DE" height="12" style={{ maxHeight: '12px'}} /></Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
